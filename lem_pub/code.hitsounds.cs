a8e65641be2adb94123d75c2b875858d4606090b
lucian patois <123527687+LGPatois@users.noreply.github.com>
2023-04-17 11:00:16 -0700

    Update code.hitsounds.cs
    jumped the gun a bit since I'm not testing
    
    we weren't checking for client Id we were using object Id for shooter so menu toggle should work now plus stock similar sound so everyone hears it
    
    also, disabled server toggle control so need to add back (suggest no) or leave on (get rid of menu options for admin) and let clients choose along with notification of what sound we're using so they can change it

--- lem_pub/code.hitsounds.cs
+++ lem_pub/code.hitsounds.cs
@@ -2,13 +2,12 @@ Attachment::AddBefore("Player::onDamage",	"HitSounds::OnDamage");
 
 function HitSounds::OnDamage(%this, %type, %value, %pos, %vec, %mom, %vertPos, %quadrant, %object)
 {
-	if ($HitSounds::enabled)
+	%shooterClient = Player::getClient(%object);
+	
+	if (%shooterClient.hitSoundsDisabled || %type == 0)
+		return;
+	else
 	{
-		//damage from impact etc.
-		if(%type == 0) 
-			return;
-
-		%shooterClient = %object;
 		%damagedClient = Player::getClient(%this);
 		
 		// Team damage not on self
@@ -16,7 +15,9 @@ function HitSounds::OnDamage(%this, %type, %value, %pos, %vec, %mom, %vertPos, %
 			
 			%damagedClientTeam = Client::getTeam(%damagedClient); // move these inside
 			%shooterClientTeam = Client::getTeam(%shooterClient);
-			Client::sendMessage(%shooterClient, 0, "~C_BuySell.wav");
+			
+			if(%shooterClientTeam != %damagedClientTeam)
+				Client::sendMessage(%shooterClient, 0, "~C_BuySell.wav");
 		}
 	}
 }
